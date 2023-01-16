1ff42d57058e97d97dc3ce03f73f86c1f7510371
QWERTYpy <burtsevmv@mail.ru>
2023-01-16 15:27:34 +0300

    Добавлены комментарии

--- root.py
+++ root.py
@@ -3,14 +3,13 @@ import tkinter as tk
 from main_frame import MainFrame
 from main_menu import MainMenu
 from info_frame import InfoFrame
-from ping3 import ping
-import threading
-import time
-import datetime
 
+
+# Создаем словарь для хранения созданных объектов
 dict_object = {}
+# Создаем словарь для хранения объектов на удаление
 del_object = {}
-# dict_object['a']=1
+
 root = tk.Tk()
 root.title(("Ping Master - v.1.0"))
 root.geometry("1210x608+100+100")  # Создаем окно
@@ -20,11 +19,8 @@ root.configure(background='#ffffff')  # Устанавливаем цвет фо
 map = MainFrame(root, 1000, 560, dict_object, del_object)
 info = InfoFrame('info', root, map, dict_object, 1200, 20)  # Инфополе внизу слева
 stat = InfoFrame('stat', root, map, dict_object, 1200, 20)  # Статистика внизу
-obj_info = InfoFrame('obj', root, map, dict_object, 180, 180) # Информация справа
-text_ping = InfoFrame('ping', root, map, dict_object, 180, 400, obj_info, info, stat)
+obj_info = InfoFrame('obj', root, map, dict_object, 180, 180)  # Информация справа
+text_ping = InfoFrame('ping', root, map, dict_object, 180, 400, obj_info, info, stat)  # Информация о пинге
 mm = MainMenu(root, map, info, dict_object, del_object)
-# threading.Thread(target=ping_object).start()
-# threading.Thread(target=watchdog_timer).start()
 root.mainloop()  # Запускаем отображение
 sys.exit()
-# print(dict_object)
