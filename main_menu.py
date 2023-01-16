13ef909db4afd8636032053ec93a67f12fcfc872
QWERTYpy <burtsevmv@mail.ru>
2023-01-16 16:08:38 +0300

    Добавлены комментарии

--- main_menu.py
+++ main_menu.py
@@ -1,56 +1,54 @@
-# Создание объектов
-import os.path
+# Верхнее меню
 import tkinter as tk
-from PIL import Image, ImageTk
-# from device import Device
 import saveload as sl
 from description import Descr
 
+
 class MainMenu:
     def __init__(self, root, map, info, dict_object, del_object):
-        self.info = info
-        self.root = root
-        self.main_canvas = map.main_canvas
-        self.map = map
-        self.dict_object = dict_object
-        self.del_oblect = del_object
+        self.info = info  # Ссылка на инфофрейм
+        self.root = root  # Ссылка на головное окно
+        self.main_canvas = map.main_canvas  # Ссылка на канву
+        self.map = map  # Ссылка на главный фрейм
+        self.dict_object = dict_object  # Ссылка на словарь объектов
+        self.del_oblect = del_object  # Ссылка на словарь удаляемых объектов
         # Создаем меню
         self.main_menu = tk.Menu(root)
         root.config(menu=self.main_menu)
+        # Связываем пункты меню с событиями
         self.main_menu.add_command(label="Сохранить", command=self.save_object)
         self.main_menu.add_command(label="Удалить", command=self.del_object)
         self.main_menu.add_command(label="Редактировать", command=self.edit_object)
         self.main_menu.add_command(label="Обновить", command=self.reboot_object)
 
     def reboot_object(self):
+        # Включаем флаг на перезагрузку
         self.map.reboot_ping = True
 
     def save_object(self):
         # Сохраняем объекты
         sl.save_ini(self.dict_object)
+        # Выводим служебное сообщение внизу слева
         self.info.title_left_down_text.set("Сохранено")
 
     def del_object(self):
         # Удаляем объекты с канвы
         for _ in self.del_oblect.keys():
-            self.dict_object.pop(_)
-            self.main_canvas.delete(self.del_oblect[_])
-            self.main_canvas.delete(_)
-        self.del_oblect.clear()
+            self.dict_object.pop(_) # Удаляем из словаря
+            self.main_canvas.delete(self.del_oblect[_])  # Удаляем прямоугольник выделения
+            self.main_canvas.delete(_)  # Удаляем объект
+        self.del_oblect.clear()  # Очищаем словарь
+        # Выводим служебное сообщение внизу слева
         self.info.title_left_down_text.set("Удалено")
 
     def edit_object(self):
         # Редактирование описания объектов
         for obj in self.del_oblect.keys():
-            # self.main_canvas.itemconfig(obj, fill="green")
             descr = Descr(self.root, ip_adr=self.dict_object[obj].ip_adr, descr=self.dict_object[obj].descr)
             _, x, y = self.root.geometry().split('+')
             descr.geometry(f"200x180+{int(x) + 100}+{int(y)+100}")
-            # print(self.dict_object[obj].ip_adr)
-            # print(obj)
             descr.grab_set()
             descr.wait_window()
-            # # print(descr.button)
             # Если в форме была нажата кнопка сохранить
             if descr.button:
                 self.dict_object[obj].ip_adr = descr.ip_adr
